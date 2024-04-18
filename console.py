6ec2e262a8e665e0428639dda98cf36d4b93f0f3
shaimaa <shaimafikry@yahoo.com>
2024-04-18 02:33:09 +0200

    first task

--- console.py
+++ console.py
@@ -125,7 +125,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return
         # sepreare paramteres, put it into dictionary
-        parameters = dict()
+        new_instance = HBNBCommand.classes[cls_name]()
         for i in d_args[1:]:
             i = i.split('=')
             value = i[1]
@@ -135,19 +135,17 @@ class HBNBCommand(cmd.Cmd):
             if value[0] == '"':
                 value = value.replace('"', "")
                 if '"' in value:
-                    pass
+                    value = value.replace('"', '\"')
             # case float
             elif '.' in value:
                 value = float(value)
             # case integer
             else:
                 value = int(value)
-            parameters[i[0]] = value
-        # print(parameters)
-        new_instance = HBNBCommand.classes[cls_name](parameters)
-        storage.save()
+            para = i[0]
+            new_instance.__dict__[para] = value
+        HBNBCommand.classes[cls_name].save(new_instance)
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
