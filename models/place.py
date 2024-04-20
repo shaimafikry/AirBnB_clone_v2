5823a6b52b94b4542aba2bc7d8ac570f8b08a040
shaimaa <shaimafikry@yahoo.com>
2024-04-20 04:31:48 +0200

    task 6

--- models/place.py
+++ models/place.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
+from sqlalchemy.orm import relationship
+from models.engine.file_storage import FileStorage
 
-
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    reviews = relationship("Review", backref="place", cascade="all, delete")
+    place_amenity = Table("place_amenity", Base.metadata,
+        Column("place_id", String(60), ForeignKey("places.id"), primary_key=True,  nullable=False),
+        Column("amenity_id",String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False )
+        )
+    amenities = relationship("Amenity", secondary="place_amenity", backref= "places", viewonly=False)
     city_id = ""
     user_id = ""
     name = ""
@@ -16,3 +35,29 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
+
+    def reviews(self):
+        """getter attribute reviews that returns the list of Review instances with place_id equals to the current Place.id => It will be the FileStorage relationship between Place and Review"""
+        from models.engine.file_storage import FileStorage
+        reviews_list = []
+        data = FileStorage.all(Place)
+        for i in data:
+            if self.id == i['place_id']:
+                reviews_list += [i['review']]
+        return reviews_list
+
+#still workin here
+    @property
+    def amenities(self):
+        """handles append method for adding an Amenity.id to the attribute amenity_ids. This method should accept only Amenity object, otherwise, do nothing."""
+        from models.amenity import Amenity
+        amen_list = []
+        data = FileStorage.all(Amenity)
+        return amen_list
+    @amenities.setter
+    def amenities():
+        
+
+
+# Getter attribute amenities that returns the list of Amenity instances based on the attribute amenity_ids that contains all Amenity.id linked to the Place
+# Setter attribute amenities that handles append method for adding an Amenity.id to the attribute amenity_ids. This method should accept only Amenity object, otherwise, do nothing.
--- models/state.py
+++ models/state.py
@@ -2,11 +2,27 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    # Use backref to automatically create a 'state' attribute in the City class
+    cities = relationship("City", backref='state', cascade="all, delete")
+
     name = ""
+
+    def cities(self):
+        """getter attribute cities that returns the list of City instances
+        with state_id equals to the current State.id
+        => It will be the FileStorage relationship between State and City"""
+        from models.engine.file_storage import FileStorage
+        cities_list = []
+        data = FileStorage.all(City)
+        for i in data:
+            if self.id == i['state_id']:
+                cities_list += [i['name']]
+        return cities_list
