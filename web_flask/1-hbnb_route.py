c0d8f9e4450db855bc54c4511125e8302339fce9
foash-111 <mr2221@fayoum.edu.eg>
2024-05-19 19:01:28 +0300

    corner stone of flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""my first module"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """hello world from the root"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbhb():
+    """hbnb"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    # debug=True, which will be beneficial if you plan to make changes
+    # and want the server to automatically reload
+    # and provide helpful debugging information.
