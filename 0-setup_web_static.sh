8717578024a1ed571916c0d9d596c5c0ba525c3c
shaimaa <shaimafikry@yahoo.com>
2024-05-21 00:48:28 +0300

    edit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Install Nginx if it not already installed
-sudo apt update
-sudo apt install -y nginx
+sudo apt-get update
+sudo apt-get install -y nginx
 # Create the folder /data/ if it doesn’t already exist
 # Create the folder /data/web_static/ if it doesn’t already exist
 # Create the folder /data/web_static/releases/ if it doesn’t already exist
@@ -11,10 +11,11 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 # Create a fake HTML file /data/web_static/releases/test/index.html
 # (with simple content, to test your Nginx configuration)
-echo "Hello World" > /data/web_static/releases/test/index.html
+sudo touch /data/web_static/releases/test/index.html
+echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
 # Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
 # If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 # Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist).
 # This should be recursive; everything inside should be created/owned by this user/group
 sudo chown -R ubuntu:ubuntu /data/
