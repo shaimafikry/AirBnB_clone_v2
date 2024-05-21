42b762b5495b622a6e6d85062eca82cb291deaa5
shaimaa <shaimafikry@yahoo.com>
2024-05-21 04:04:22 +0300

    edit

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# Fabric script (based on the file 1-pack_web_static.py)
+# that distributes an archive to your web servers
+# using the function do_deploy:
+from fabric.api import *
+from datetime import *
+from fabric.contrib.files import exists
+env.hosts = ['35.175.63.155', '54.226.26.233']
+
+
+def do_deploy(archive_path):
+    """to deploy an archeive"""
+    # Returns False if the file at the path archive_path doesn’t exist
+    if not exists(archive_path):
+        return False
+    try:
+        # Upload the archive to the /tmp/ directory of the web server
+        put(f"{archive_path}", f"/tmp/{archive_path}")
+        # Uncompress the archive to the folder
+        # /data/web_static/releases/<archive filename without extension>
+        # on the web server
+        archive = archive_path.split('/')[-1]
+        archive_wt_ext = archive.split('.')[0]
+        path = "/data/web_static/releases"
+        run(f"tar -xvf /tmp/{archive} {path}/{archive_wt_ext}/")
+        # Delete the archive from the web server
+        sudo(f"rm {archive}")
+        # Delete the symbolic link /data/web_static/current from the web server
+        sudo("rm /data/web_static/current")
+        # Create a new the symbolic link /data/web_static/current
+        # on the web server, linked to the new version of
+        # your code /data/web_static/releases/archive filame without extension
+        sudo(f"ln -s -f {path}/{archive_wt_ext} /data/web_static/current ")
+        return True
+    except RuntimeError:
+        return False
