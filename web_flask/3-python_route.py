d5e51270edcc0df7bf9f562386c7836efc9fc874
shaimaa <shaimafikry@yahoo.com>
2024-06-16 18:48:51 +0300

    edit

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Write a script that starts a Flask web application:
-Your web application must be listening on 0.0.0.0,
-port 5000
+Your web application must be listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
-/c/<text>: display “C ” followed by the value of
-the text variable (replace underscore _ symbols with a space )
+/c/<text>: display “C ”, followed by the value of the
+text variable (replace underscore _ symbols with a space )
+/python/<text>: display “Python ”, followed by the value of the
+text variable (replace underscore _ symbols with a space )
+The default value of text is “is cool”
 You must use the option strict_slashes=False in your route definition"""
 
 from flask import Flask
@@ -27,6 +29,11 @@ def bnb():
 @app.route('/c/<text>', strict_slashes=False)
 def hello_text(text):
     return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text="is cool"):
+    return f"Python {text.replace('_', ' ')}"
 
 
 if __name__ == '__main__':
