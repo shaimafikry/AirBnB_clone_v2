aafa65756d2634452e2434b95249afd10178ba2f
shaimaa <shaimafikry@yahoo.com>
2024-06-16 19:45:57 +0300

    edit

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ”, followed by the value of the text variable
+(replace underscore _ symbols with a space )
+/python/(<text>): display “Python ”, followed by the value of the text variable
+(replace underscore _ symbols with a space )
+The default value of text is “is cool”
+/number/<n>: display “n is a number” only if n is an integer
+You must use the option strict_slashes=False in your route definition"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_bnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def bnb():
+    return "HBNB!"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_text(text):
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/', defaults={"data": "is cool"}, strict_slashes=False)
+@app.route('/python/<data>', strict_slashes=False)
+def hello_python(data="is_cool"):
+    return f"Python {data.replace('_', ' ')}"
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def hello_number(n):
+    try:
+        n = int(n)
+        return f"{n} is a number"
+    except ValueError:
+        return "Not a valid integer", 400
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
