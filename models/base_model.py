42f8c4d31eaf7770c91a7977f429eadb9a536da3
shaimaa <shaimafikry@yahoo.com>
2024-06-18 04:12:52 +0300

    editing my sql

--- models/base_model.py
+++ models/base_model.py
@@ -23,11 +23,20 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            if kwargs.get('created_at') == None:
+                self.created_at = datetime.now()
+            else:
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            if kwargs.get('updated_at') == None:
+                self.updated_at = datetime.now()
+            else:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if kwargs.get('id') == None:
+                self.id = str(uuid.uuid4())
+            if kwargs.get('__class__') != None:
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
